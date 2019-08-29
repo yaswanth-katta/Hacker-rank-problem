ad5fac62259ed60f007ed80d03ca09aaeb420c4d
yaswanth katta <44583720+yaswanth-katta@users.noreply.github.com>
2019-08-29 16:30:02 +0530

    Create Halloween Scale.c

---
+++ Halloween Scale.c
@@ -0,0 +1,20 @@
+#include <math.h>
+#include <stdio.h>
+
+int howManyGames(int p, int d, int m, int s) {
+  int counter = 0, currentPrice = p;
+  while (s >= currentPrice) {
+    counter++;
+    s -= currentPrice;
+    currentPrice = (currentPrice - d) >= m ? currentPrice - d : m;
+  }
+  return counter;
+}
+
+int main() {
+  int p, d, m, s;
+  scanf("%i %i %i %i", &p, &d, &m, &s);
+  int answer = howManyGames(p, d, m, s);
+  printf("%d\n", answer);
+  return 0;
+}
