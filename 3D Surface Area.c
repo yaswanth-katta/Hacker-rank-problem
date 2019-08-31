c7970a18d9534eb1505702b5128ec0985e192de5
yaswanth katta <44583720+yaswanth-katta@users.noreply.github.com>
2019-08-31 11:16:12 +0530

    Update 3D Surface Area.c
    To added question for this commit

--- 3D Surface Area.c
+++ 3D Surface Area.c
@@ -1,5 +1,44 @@
-#include <stdio.h>
+/*Madison, is a little girl who is fond of toys. Her friend Mason works in a toy manufacturing factory .
+Mason has a 2D board  of sizeH x W with H rows and W columns. The board is divided into cells of size  with each cell indicated by
+it's coordinate(i,j) . The cell (i,j) has an integer Aij written on it. 
+To create the toy Mason stacks  number of cubes of size  on the cell .
 
+Given the description of the board showing the values of  and that the price of the toy is equal to the 3d surface area 
+find the price of the toy.
+
+Input Format
+
+The first line contains two space-separated integers  and  the height and the width of the board respectively.
+
+The next  lines contains  space separated integers. The  integer in  line denotes .
+
+Output Format
+Print the required answer, i.e the price of the toy, in one line.
+
+Sample Input 0
+
+1 1
+1
+Sample Output 0
+6
+Explanation 0
+
+The surface area of  cube is 6.
+
+Sample Input 1
+
+3 3
+1 3 4
+2 2 3
+1 2 4
+Sample Output 1
+60
+Explanation 1
+The sample input corresponds to the figure described in problem statement.*/
+
+/*HERE IS THE CODE FOR THE ABOVE PROBLEM*/
+
+#include <stdio.h>
 int T[200][200];
 
 int main() {
