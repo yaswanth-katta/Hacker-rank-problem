c4e8f2a508bc3bea7b6706174207909c6a15427e
yaswanth katta <44583720+yaswanth-katta@users.noreply.github.com>
2021-02-09 18:13:41 +0530

    Create Validating Credit card number.py

---
+++ Validating Credit card number.py
@@ -0,0 +1,19 @@
+                          /*problem statement*/
+    https://www.hackerrank.com/challenges/validating-credit-card-number/problem
+    
+
+
+             /*solution for the above problem*/
+import re
+
+for _ in range(int(input())):
+    num = input()
+
+    ok1 = bool(re.match(r"^[456]\d{15}$", num))
+    ok2 = bool(re.match(r"^[456]\d{3}\-\d{4}\-\d{4}\-\d{4}$", num))
+    num = num.replace("-", "")
+    ok3 = bool(re.match(r"(?!.*(\d)(-?\1){3})", num))
+    if (ok1 or ok2) and ok3:
+        print("Valid")
+    else:
+        print("Invalid")
